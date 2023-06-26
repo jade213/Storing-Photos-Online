21db75fff92a0fc8f77de70944f2929d097e1aee
jadec <j.coulson5@newcastle.ac.uk>
2023-06-26 18:42:37 +0100

    add images, css, html and python files/folders to create parallax scroll

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('hello world')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def start():
+    return 'hello world'
+
+if __name__ == '__main__':
+    start()
